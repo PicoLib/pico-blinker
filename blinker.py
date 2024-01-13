b8a62a79f1f38b3a138327c12aa18c5dad649e79
Mohsen <47813580+mosnfar@users.noreply.github.com>
2024-01-13 15:36:39 +0330

    Rewrite the main class

--- blinker.py
+++ blinker.py
@@ -1,24 +1,29 @@
 import utime
 from machine import Pin
 
+# Configure led pin
 BUILD_IN_LED_PIN = 25
 led_onboard = Pin(BUILD_IN_LED_PIN, Pin.OUT)
 
-def blink_led_loop(x):
-    while True:
-        led_onboard.on()
-        utime.sleep(x)
-        led_onboard.off()
-        utime.sleep(x)
+class Led:
+    """ Using this class you can access onboard led quickly """
 
-def blink_led(y):
-    led_onboard.on()
-    utime.sleep(y)
-    led_onboard.off()
-    utime.sleep(y)
+    def switch(z: bool):
+        """ This function will turn on or off the led """
+        if z == 1:
+            led_onboard.on()
+        else:
+            led_onboard.off()
 
-def led(z):
-    if z == 1:
-        led_onboard.on()
-    else:
-        led_onboard.off()
+    def blink(delay: float, off_delay=True):
+        """ This function will turn on for given time and turn off the led after that """
+        self.switch(1)
+        utime.sleep(delay)
+        self.switch(0)
+        if off_delay == True:
+            utime.sleep(delay)
+        else: pass
+
+    def loop(interval: float):
+        while True:
+            self.blink(interval)
